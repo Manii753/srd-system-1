833e5e1efa840e9dda9fa81632c173ce40f36421
Aziz Ahmed <azizahmed10088@gmail.com>
2026-03-05 08:18:12 +0500

    connection resolved

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import dns from 'dns';
 
-
+// Use Google's DNS servers to resolve MongoDB SRV records
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 dotenv.config();
 
