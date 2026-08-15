e9ae25e0590a0103dab9bd617b321c6594333a88
Aziz Ahmed <azizahmed10088@gmail.com>
2026-08-15 12:33:20 +0500

    ui

--- src/components/DepartmentPanelExcel.js
+++ src/components/DepartmentPanelExcel.js
@@ -2460,7 +2460,7 @@ export default function DepartmentPanelExcel({
           </div>
 
           {/* Approval Sections - Rendered inside grid on last page */}
-          {currentSection?.includeApprovals && (
+          {/* {currentSection?.includeApprovals && (
             <>
               {!readOnly && (
                 <div className="border-transparent p-3">
@@ -2528,7 +2528,7 @@ export default function DepartmentPanelExcel({
                 </div>
               )}
             </>
-          )}
+          )} */}
         </div>
         {/* Pagination Controls */}
         {sections.length > 1 && (
